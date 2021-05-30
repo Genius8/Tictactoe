91b829d865d3cbc7c0b5895e8a89db8a68b14efb
Genius8 <erasmusokiror111@gmail.com>
2021-05-30 16:56:34 +0300

    Fix the errors

--- bin/main.rb
+++ bin/main.rb
@@ -2,16 +2,22 @@
 #board representation
 class TicTacToeUI
   def initialize
-    @board =Array.new (9," ")
+    @board =Array.new (9, " ")
     
   end
- WINS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[6,4,2],[0,4,8],]
+ WIN_COMBS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[6,4,2],[0,4,8],]
 def board_display
-  puts "#{@board[0]}|#{@board[1]}|#{@board[2]}"
+  puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
   puts "-----------"
-  puts "#{@board[3]}|#{@board[4]}|#{@board[5]}"
+  puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
   puts "-----------"
-  puts "#{@board[6]}|#{@board[7]}|#{@board[8]}"
+  puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
 end
-
+def input (user_input)
+  @choise = user_input.to_i-1
+end
+def movement(input,player="x")
+  @board[input] = player
+end
+end
 end
